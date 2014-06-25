808bbea40d9a9e43a14ba80771e4155d9dfee5e9
Khemraj Thapa <khyamay@gmail.com>
2014-06-25 21:30:25 +0100

    added userSchema

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ var async = require('async');
 var crypto = require('crypto');
 
 var app = express();
+mongoose.connect('localhost');
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -32,6 +33,42 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(session({secret: 'session secret key'}));
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+var userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date
+});
+
+var User = mongoose.model('User', userSchema);
+userSchema.pre('save', function (next){
+	var user = this;
+	var SALT_FACTOR = 5;
+
+	if(!user.isModified('password')) return next();
+
+	bcrypt.genSalt(SALT_FACTOR, function (err, salt){
+		if (err) return next(err);
+
+		bcrypt.hash(user.password, salt, null, function (err, hash){
+			if (err) return next(err);
+			user.password = hash;
+			next();
+		});
+	});
+});
+
+userSchema.methods.comparePassword = function (candidatePassword, cb){
+		bcrypt.compare(candidatePassword, this.password, function (err, isMatch){
+			if (err) return cb(err);
+			cb(null, isMatch);
+	});
+}
+
+
 
 
 
